a03369d4f7be4ecfb90412dc418bb88b3a9a7de9
jogo3000 <uusitalo.matti@gmail.com>
2014-05-03 15:43:51 +0300

    Created tests for GuildCommandProcessor classs

---
+++ src/batmudgoalsplugin/GuildCommandProcessor.java
@@ -0,0 +1,30 @@
+package batmudgoalsplugin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.regex.Matcher;
+
+/**
+ * Catches guild info commands, e.g. 'barbarian info' and stores the guild name
+ * from that command.
+ */
+public class GuildCommandProcessor extends AbstractCommandProcessor {
+
+	private Collection<IGuildNameListener> listeners;
+
+	public GuildCommandProcessor(IGuildNameListener... listeners) {
+		super("(?:.*;)*\\s*(.+)\\sinfo\\s*;*.*");
+		this.listeners = new ArrayList<IGuildNameListener>(
+				Arrays.asList(listeners));
+	}
+
+	@Override
+	protected boolean process(Matcher m) {
+		for (IGuildNameListener l : listeners) {
+			l.setGuild(m.group(1).trim().toLowerCase());
+		}
+		return false; // Always forward the command to client
+	}
+
+}
