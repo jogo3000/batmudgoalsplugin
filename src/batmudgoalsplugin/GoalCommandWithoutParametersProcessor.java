441f241aa3473b3934409676d622bee956aa4571
jogo3000 <uusitalo.matti@gmail.com>
2014-05-03 16:58:36 +0300

    Created test for GoalCommandWithoutParametersProcessor

--- src/batmudgoalsplugin/GoalCommandWithoutParametersProcessor.java
+++ src/batmudgoalsplugin/GoalCommandWithoutParametersProcessor.java
@@ -1,5 +1,6 @@
 package batmudgoalsplugin;
 
+import java.util.Collection;
 import java.util.regex.Matcher;
 
 import batmudgoalsplugin.data.BatMUDGoalsPluginData;
@@ -10,17 +11,24 @@ import com.mythicscape.batclient.interfaces.ClientGUI;
  * Catches goal command without parameters and prints list of possible goal
  * skills
  */
-class GoalCommandWithoutParametersProcessor extends
-		AbstractCommandProcessor {
-	public GoalCommandWithoutParametersProcessor(ClientGUI clientGUI, BatMUDGoalsPluginData data) {
+class GoalCommandWithoutParametersProcessor extends AbstractCommandProcessor {
+	public GoalCommandWithoutParametersProcessor(ClientGUI clientGUI,
+			BatMUDGoalsPluginData data) {
 		super("\\s*goal\\s*", clientGUI, data);
 	}
 
 	@Override
 	protected boolean process(Matcher m) {
-		for (String skillName : data.getStoredSkills())
-			printMessage("%s%s", skillName,
-					data.isGoalSkill(skillName) ? " (*)" : "");
+
+		Collection<String> storedSkills = data.getStoredSkills();
+		if (storedSkills.isEmpty()) {
+			printMessage("No data.");
+		} else {
+
+			for (String skillName : storedSkills)
+				printMessage("%s%s", skillName,
+						data.isGoalSkill(skillName) ? " (*)" : "");
+		}
 		return true;
 	}
 }
---
+++ test/batmudgoalsplugin/GoalCommandWithoutParametersProcessorTest.java
@@ -0,0 +1,33 @@
+package batmudgoalsplugin;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+
+import batmudgoalsplugin.data.BatMUDGoalsPluginData;
+
+import com.mythicscape.batclient.interfaces.ClientGUI;
+
+public class GoalCommandWithoutParametersProcessorTest {
+
+	private ClientGUI verifyGoalOutput(BatMUDGoalsPluginData data) {
+		ClientGUI mock = mock(ClientGUI.class);
+		new GoalCommandWithoutParametersProcessor(mock, data).receive("goal");
+
+		final ClientGUI verify = verify(mock);
+		return verify;
+	}
+
+	public void testGoalCommandListsAvailableGoalSkills_noCommands() {
+		verifyGoalOutput(new BatMUDGoalsPluginData()).printText("generic",
+				"No data.\n");
+	}
+
+	@Test
+	public void testGoalCommandListsAvailableGoalSkills() throws Exception {
+		BatMUDGoalsPluginData data = new BatMUDGoalsPluginData();
+		data.setSkillCost("attack", 1, 1);
+		verifyGoalOutput(data).printText("generic", "attack\n");
+	}
+}
