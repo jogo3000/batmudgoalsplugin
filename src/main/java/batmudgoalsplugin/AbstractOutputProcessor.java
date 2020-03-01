7449e14aee54f75bcf9cdf6ef61ad4bc152c6d0d
Matti Uusitalo <uusitalo.matti@gmail.com>
2020-03-01 07:15:33 +0200

    Use final whenever possible

--- src/main/java/batmudgoalsplugin/AbstractOutputProcessor.java
+++ src/main/java/batmudgoalsplugin/AbstractOutputProcessor.java
@@ -13,30 +13,30 @@ abstract class AbstractOutputProcessor {
      * Extending classes should call this constructor to provide a regular
      * expression which is matched against command output. On matches, the extending
      * class will receive a call to its process method with the {@link Matcher}
-     * 
+     *
      * @param regexp
      * @param plugin optional
      */
-    public AbstractOutputProcessor(String regexp) {
+    public AbstractOutputProcessor(final String regexp) {
         this.pattern = Pattern.compile(regexp, Pattern.CASE_INSENSITIVE);
     }
 
     /**
      * Extending classes should not need to override this method.
-     * 
+     *
      * @param input
      */
-    public final void receive(String input) {
+    public final void receive(final String input) {
         decideProcess(pattern.matcher(input));
     }
 
     /**
      * Extending classes may override this method to provide logic of deciding when
      * to process
-     * 
+     *
      * @param m
      */
-    protected void decideProcess(Matcher m) {
+    protected void decideProcess(final Matcher m) {
         if (m.matches()) {
             process(m);
         }
@@ -45,20 +45,20 @@ abstract class AbstractOutputProcessor {
     /**
      * Extending classes implement the logic what happens when the regexp is matched
      * in this method.
-     * 
+     *
      * @param m
      */
     protected abstract void process(Matcher m);
 
     /**
      * Removes extra whitespaces and puts to lowercase
-     * 
+     *
      * @param originalSkillName
      * @return normalized skill name
      */
-    protected String normalizeSkillName(String originalSkillName) {
-        StringBuilder sb = new StringBuilder();
-        for (String s : originalSkillName.split("\\s+")) {
+    protected String normalizeSkillName(final String originalSkillName) {
+        final StringBuilder sb = new StringBuilder();
+        for (final String s : originalSkillName.split("\\s+")) {
             sb.append(s);
             sb.append(" ");
         }
